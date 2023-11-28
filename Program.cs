ef0018b0fec35eeefc43982181223950ce747eca
bhiseamar811@gmail.com <bhiseamar811@gmail.com>
2023-11-28 13:39:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿namespace ConDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsonuntCount();
+
+            //StringManupulation();
+
+            //Console.WriteLine(" Main function is called");
+
+            //Program p = new Program();
+            //Console.WriteLine(" value of i is ="+p.i+p.b);
+            //Console.WriteLine("value of b is ="+p.b);
+        }
+        
+        
+
+            //static void ShowfullName()
+            //{
+            //    string FirstName, LastName, FullName;
+
+            //    Console.WriteLine("Please enter first Name");
+            //    FirstName = Console.ReadLine();
+
+            //    Console.WriteLine("Please enter Last Name");
+            //    LastName = Console.ReadLine();
+
+            //    Console.WriteLine("This is fullName ="+ FirstName+" "+ LastName);
+            //}
+
+            //static void VovelesCounter()
+            //{
+            //    Console.WriteLine("Please enter any string");
+            //    var Str = Console.ReadLine();
+
+            //    int VowelesCounter = 0;
+
+            //    foreach (var item in Str)
+            //    {
+            //       if(item == 'A'|| item == 'E' || item == 'I' || item == 'O' || item == 'U' ||
+            //            item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u')
+            //        {
+            //            VowelesCounter++;
+            //        }
+
+            //    }
+            //    Console.WriteLine("Total Vowles Count in String is   = " + VowelesCounter);
+            //}
+
+
+            //static void CountLenthOfString()
+            //{
+            //    Console.WriteLine("please enter anyu string ");
+            //    var Str = Console.ReadLine();
+
+            //    int LenthCount = 0;
+
+            //    foreach (var item in Str)
+            //    {
+            //        LenthCount++;
+            //    }
+            //    Console.WriteLine("total count of the string is =" + LenthCount);
+
+            //}
+        //    static void ConsonuntCount()
+        //{
+        //    Console.WriteLine("please enter any string ");
+        //    var Str = Console.ReadLine();
+
+        //    int ConsonuntCount = 0;
+
+        //    foreach (var item in Str)
+        //    {
+        //        if(!(item=='A'|| item == 'E' || item == 'I' || item == 'O' || item == 'U' ||
+        //            item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u'||item==' '))
+        //        {
+        //            ConsonuntCount++;
+        //        }
+
+        //    }
+        //    Console.WriteLine("total ConsonuntCount is =" + ConsonuntCount);
+        //}
+
+          
+        
+        //static void StringManupulation()
+        //{
+        //    Console.WriteLine("please enter any string ");
+        //    var Str = Console.ReadLine();
+
+
+        //    int SpaceCount = 0;
+        //    foreach (Char ch in Str)
+        //    {
+        //        if (ch == ' ')
+        //        {
+        //            SpaceCount++;
+        //        }
+        //    }
+            
+        //    Console.WriteLine("Total Space Count In The string =" + SpaceCount);
+        //    Console.WriteLine("Total Word Count In The String= "+ (SpaceCount+1));
+
+        //}
+    }
+}
